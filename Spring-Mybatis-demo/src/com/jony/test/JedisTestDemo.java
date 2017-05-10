e8039051a3058ef89c7dcc8c657f3e77bf365dcb
gaoning <gnpublic@163.com>
2017-05-10 17:19:19 +0800

    test

--- Spring-Mybatis-demo/src/com/jony/test/JedisTestDemo.java
+++ Spring-Mybatis-demo/src/com/jony/test/JedisTestDemo.java
@@ -28,14 +28,16 @@ public class JedisTestDemo {
 	            JedisPoolConfig config = new JedisPoolConfig();
 	            //控制一个pool可分配多少个jedis实例，通过pool.getResource()来获取；
 	            //如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
-	            config.setMaxTotal(-1);
+	            
 	            //控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
 	            config.setMaxIdle(5);
 	            //表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
-	            config.setMaxWaitMillis(1000*100);
+	            config.setMaxWait(1000*100);
 	            //在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
 	            config.setTestOnBorrow(true);
-	            pool = new JedisPool(config, "127.0.0.1", 6379);
+	          //(Config poolConfig, String host, int port, int timeout, String password)
+	            pool = new JedisPool(config, "127.0.0.1", 6379,300,"test123");
+//	            pool = new JedisPool(config, "127.0.0.1", 6379);
 	        }
 	        return pool;
 	    }
@@ -366,7 +368,7 @@ public class JedisTestDemo {
 //		sortedSet();
 //		testHsh();
 //		testOther();
-		jedis.select(3);
+//		jedis.select(3);
 //		HashMap map = (HashMap) jedis.hget("detailInstMap");
 //		String a = map.toString();
 //		CreateFileUtils.writeData2file("D:/", "map.txt", a, 1024);
